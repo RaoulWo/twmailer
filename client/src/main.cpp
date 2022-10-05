3ca7ed8bb7d61101d5f6ac128e98b3c661ab7833
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-10-05 17:32:37 +0200

    Split project into server and client

---
+++ client/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am a Client!" << std::endl;
+
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am a Server!" << std::endl;
+
+    return 0;
+}
