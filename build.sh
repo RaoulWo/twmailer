48c8df36b148bc233b6139843b272efacf189789
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-10-12 18:29:57 +0200

    Refactor shell scripts

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Build twmailer-client
 echo 'Build twmailer-client'
 (cd ./client && make clean && make)
+
 echo ''
 
 # Build twmailer-server
 echo 'Build twmailer-server'
 (cd ./server && make clean && make)
-echo ''
--- clean.sh
+++ clean.sh
@@ -3,9 +3,9 @@
 # Clean twmailer-client
 echo 'Clean twmailer-client'
 (cd ./client && make clean)
+
 echo ''
 
 # Clean twmailer-server
 echo 'Clean twmailer-server'
 (cd ./server && make clean)
-echo ''
