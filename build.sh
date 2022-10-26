1f5ae4a30a00033c07129f87a451568b44465a23
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-10-26 11:36:09 +0200

    Add colored output to shell scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+LIGHT_CYAN='\033[1;36m'
+NC='\033[0m'
+
 # Build twmailer-client
-echo 'Build twmailer-client'
+echo -e "${LIGHT_CYAN}Build twmailer-client${NC}"
 (cd ./client && make clean && make)
 
 echo ''
 
 # Build twmailer-server
-echo 'Build twmailer-server'
+echo -e "${LIGHT_CYAN}Build twmailer-server${NC}"
 (cd ./server && make clean && make)
